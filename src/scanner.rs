de969d6e7c4b39bca740c80cd05be8adf9f69724
Joel Bonini <joel.bonini@gmx.ch>
2026-03-30 16:33:34 +0200

    feat: implement async TCP connect scanner with tokio

---
+++ src/scanner.rs
@@ -0,0 +1,12 @@
+use std::{net::{IpAddr, SocketAddr}, time::Duration};
+use tokio::time::timeout;
+use tokio::net::TcpStream;
+
+
+
+pub async fn scan(ip_str: &str, port: u16) -> bool {
+    let ip: IpAddr = ip_str.parse().unwrap();
+    let socket = SocketAddr::new(ip, port);
+
+    matches!(timeout(Duration::from_secs(2), TcpStream::connect(socket)).await, Ok(Ok(_)))
+}
